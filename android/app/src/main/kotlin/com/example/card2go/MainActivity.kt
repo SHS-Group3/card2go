79df8b682cd9bb23ffda6c795fe650c5dc8947a2
INTERN5-GROUP3 <INTERN5-GROUP3@FDSAP-INTERN5.local>
2024-02-27 11:34:19 +0800

    login_page.dart

---
+++ android/app/src/main/kotlin/com/example/card2go/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.card2go
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obsecureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obsecureText
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obsecureText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          fillColor: Colors.grey,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
